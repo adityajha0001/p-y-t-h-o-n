887f13f1a9f029a3e8b40e8d0a952485cb94232e
adityajha0001 <rockadi2407@gmail.com>
2024-03-04 18:00:56 +0530

    day04 of python

--- Problems/Problem02.py
+++ Problems/Problem02.py
@@ -1,39 +1,42 @@
 
 Bigdata ={}
-user = {}
-
-name = input("Enter your name :")
-age  = input("Enter your age :")
-day  = input("Enter your day :")
 
 
-user["Day"] = day
-user["Age"] = int(age)
+#how many tickets you want to buy
+n = input("enter the number of ticket you want :")
 
-# print(user)
-
-Bigdata[name] = user
+for i in range(int(n)):
+    user = {}
+    name = input("Enter your name :")
+    age  = input("Enter your age :")
+    day  = input("Enter your day :")
+    user["Day"] = day
+    user["Age"] = int(age)
+    Bigdata[name] = user
+    
+totalPay = int(0)
 # print(Bigdata)
+for key,value in Bigdata.items():
+    life = Bigdata[key]["Age"]
+    print(life)  
+    MovieDay = Bigdata[key]["Day"]
+    print(MovieDay)
+    
+    if(life< 18):
+        ticketPrice = int(8)
+        print(f"you have to pay ${ticketPrice} only")
+        if(MovieDay == "wednesday"):
+            discount = int(2)
+            ticketPrice = ticketPrice - discount
+            print(f"Because you are booking ticket for wednesday show now you have to pay ${ticketPrice} only")
+            totalPay += ticketPrice
+    elif(life>=18):
+        ticketPrice = int(12)
+        print("you have to pay ${} only".format(ticketPrice))
+        if(MovieDay == "wednesday"):
+            discount = int(2)
+            ticketPrice = ticketPrice - discount
+            print("Because you are booking ticket for wednesday show now you have to pay ${} only".format(ticketPrice))
+            totalPay += ticketPrice
 
-life = Bigdata[name]["Age"]
-# print(age)
-
-MovieDay = Bigdata[name]["Day"]
-
-# user[value] = value
-# print(user[key])
-
-if(life< 18):
-    ticketPrice = int(8)
-    print(f"you have to pay ${ticketPrice} only")
-    if(MovieDay == "wednesday"):
-        discount = int(2)
-        ticketPrice = ticketPrice - discount
-        print(f"Because you are booking ticket for wednesday show now you have to pay ${ticketPrice} only")
-elif(life>=18):
-    ticketPrice = int(12)
-    print("you have to pay ${} only".format(ticketPrice))
-    if(MovieDay == "wednesday"):
-        discount = int(2)
-        ticketPrice = ticketPrice - discount
-        print("Because you are booking ticket for wednesday show now you have to pay ${} only".format(ticketPrice))
+print(f"your net payment is ${totalPay}")
